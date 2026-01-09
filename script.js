ed1d0b742fe7ed2f7de0f99717a78b1dc5e33c9e
Lowoxtreeme@gmailcom <emailkamu@gmail.com>
2026-01-10 00:03:12 +0700

    aku nambahin itunya pokoknya itu biar ga apa kali ya kan

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const form = document.getElementById('formkomentar');
+const daftarKomentar = document.getElementById('daftarkomentar');
 
+let komentar = JSON.parse(localStorage.getItem('komentar')) || [];
+
+function renderKomentar() {
+    daftarKomentar.innerHTML = '';
+    komentar.forEach(item => {
+        const div = document.createElement('div');
+        div.classList.add('komentar-card');
+        div.innerHTML = `
+            <strong>${item.nama}:</strong> ${item.pesan} </p>`;
+        daftarKomentar.appendChild(div);
+    });
+}
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const nama = document.getElementById('nama').value;
+    const pesan = document.getElementById('pesan').value;
+
+    komentar.push({ nama, pesan });
+    localStorage.setItem('komentar', JSON.stringify(komentar));
+    form.reset();
+    renderKomentar();
+} );
+renderKomentar();
